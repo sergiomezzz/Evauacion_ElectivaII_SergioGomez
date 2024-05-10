5770a64e6a8961b6acccc4e23e4955697e033223
sergio gomez <sergio.gomez05@uptc.edu.co>
2024-05-09 20:05:39 -0500

    intento agregar el produto a la venta

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 const express = require('express');
 const app = express();
+const productsRoutes = require('./routes/productsRoutes');
 const data = require('./data.json');
-
-const productsArray = Object.values(data.products);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', productsRoutes);
 
 app.get('/', (req, res) => {
-  res.render('index', { products: productsArray });
-});
-
-app.post('/api/sale', (req, res) => {
-  const productId = req.body.productId;
-  const quantity = req.body.quantity;
-  
-  
-  //venta del producto
-
-  console.log(`Producto ${productId} agregado a la venta con cantidad ${quantity}`);
-
-  res.send('Product added to sale');
+  res.render('index', { products: data.products, sale: req.app.locals.sale || [] });
 });
 
 app.listen(3000, () => {
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -9,8 +9,21 @@ router.get('/products', (req, res) => {
 router.post('/sale', (req, res) => {
   const productId = req.body.productId;
   const quantity = req.body.quantity;
- 
-  res.send('Product added to sale');
+
+  // Buscar el producto en los datos
+  const product = data.products[productId];
+
+  if (!product) {
+    return res.status(404).send('Product not found');
+  }
+
+  // Agregar el producto a la venta
+  let sale = req.app.locals.sale || [];
+  const saleItem = { product, quantity };
+  sale.push(saleItem);
+  req.app.locals.sale = sale;
+
+  res.json(saleItem);
 });
 
 module.exports = router;
