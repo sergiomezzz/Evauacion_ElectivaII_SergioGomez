b8af0ca1b29d341d8158dee8e0c367ff7c96f2af
sergio gomez <sergio.gomez05@uptc.edu.co>
2024-05-09 19:29:05 -0500

    avance, agrega cantidad y producto, implento tabla de bootstrap

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
-const data = require('./data.json'); // Importa los datos del archivo JSON
-const productsRoutes = require('./routes/index');
+const data = require('./data.json');
+
+const productsArray = Object.values(data.products);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
-app.use('/api', productsRoutes);
+
 app.get('/', (req, res) => {
-  res.render('index', { products: data.products });
+  res.render('index', { products: productsArray });
+});
+
+app.post('/api/sale', (req, res) => {
+  const productId = req.body.productId;
+  const quantity = req.body.quantity;
+  
+  
+  //venta del producto
+
+  console.log(`Producto ${productId} agregado a la venta con cantidad ${quantity}`);
+
+  res.send('Product added to sale');
 });
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
-
-
-
